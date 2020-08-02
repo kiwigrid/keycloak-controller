0aa3947acc177fd586f5719f49051303b9a0f768
André Bauer <monotek23@gmail.com>
2020-08-03 01:17:09 +0200

    add live log output
    Signed-off-by: André Bauer <monotek23@gmail.com>

--- .github/k8s-install.sh
+++ .github/k8s-install.sh
@@ -71,12 +71,11 @@ kubectl -n "${NAMESPACE}" get keycloaks.k8s.kiwigrid.com
 echo ""
 
 echo -e "\n##### check for errors in keycloak-controller logs #####\n"
-sleep 60
+kubectl -n "${NAMESPACE}" logs -l app.kubernetes.io/name=keycloak-controller &
+sleep 150
 if kubectl -n "${NAMESPACE}" logs -l app.kubernetes.io/name=keycloak-controller | grep -q ERROR; then
     echo "errors found in logs"
-    kubectl -n "${NAMESPACE}" logs -l app.kubernetes.io/name=keycloak-controller
     exit 1
 else
     echo "no errors found in logs"
-    kubectl -n "${NAMESPACE}" logs -l app.kubernetes.io/name=keycloak-controller
 fi
