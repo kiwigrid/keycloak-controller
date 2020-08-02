509ec0a774d07df56daa76787de1ada25598da3a
André Bauer <monotek23@gmail.com>
2020-08-03 01:14:24 +0200

    check for log errors
    Signed-off-by: André Bauer <monotek23@gmail.com>

--- .github/k8s-install.sh
+++ .github/k8s-install.sh
@@ -66,5 +66,13 @@ kubectl -n "${NAMESPACE}" get keycloakclientscopes.k8s.kiwigrid.com
 kubectl -n "${NAMESPACE}" get keycloakrealms.k8s.kiwigrid.com
 kubectl -n "${NAMESPACE}" get keycloaks.k8s.kiwigrid.com
 
-echo -e "\n##### show keycloak-controller logs #####\n"
-kubectl -n "${NAMESPACE}" logs -l app.kubernetes.io/name=keycloak-controller
+echo -e "\n##### check for errors in keycloak-controller logs #####\n"
+sleep 60
+if kubectl -n "${NAMESPACE}" logs -l app.kubernetes.io/name=keycloak-controller | grep -q ERROR; then
+    echo "errors found in logs"
+    kubectl -n "${NAMESPACE}" logs -l app.kubernetes.io/name=keycloak-controller
+    exit 1
+else
+    echo "no errors found in logs"
+    kubectl -n "${NAMESPACE}" logs -l app.kubernetes.io/name=keycloak-controller
+fi
