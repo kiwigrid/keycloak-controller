7e70efa1fc0187df9ba908d3a701aea5c0216c3b
André Bauer <monotek23@gmail.com>
2020-08-03 00:23:08 +0200

    create kind cluster before docker build
    Signed-off-by: André Bauer <monotek23@gmail.com>

--- .github/docker-build.sh
+++ .github/docker-build.sh
@@ -7,15 +7,15 @@ set -o errexit
 DOCKER_TAG="keycloak-controller:ci-snapshot"
 
 echo -e "\n##### install mvn & java #####\n"
-sudo apt-get update > /dev/null
-sudo apt-get install -y maven openjdk-11-jdk > /dev/null
+sudo apt-get update
+sudo apt-get install -y maven openjdk-11-jdk
 
 echo -e "\n##### build keycloak-controller #####\n"
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
-mvn package --quiet
+mvn package
 
 echo -e "\n##### create docker image with tag ${DOCKER_TAG} #####\n"
 docker build -t "${DOCKER_TAG}" ./target
 
 echo -e "\n##### push docker image to kind nodes #####\n"
-kind load docker-image "${DOCKER_TAG}"
+kind --name chart-testing load docker-image "${DOCKER_TAG}"
