755fbf89adfcb999ef424e66d9d6f03d4186aee5
André Bauer <monotek@users.noreply.github.com>
2020-08-03 10:24:07 +0200

    updated log output

--- .github/k8s-install.sh
+++ .github/k8s-install.sh
@@ -71,11 +71,12 @@ kubectl -n "${NAMESPACE}" get keycloaks.k8s.kiwigrid.com
 echo ""
 
 echo -e "\n##### check for errors in keycloak-controller logs #####\n"
-kubectl -n "${NAMESPACE}" logs -l app.kubernetes.io/name=keycloak-controller &
 sleep 150
 if kubectl -n "${NAMESPACE}" logs -l app.kubernetes.io/name=keycloak-controller | grep -q ERROR; then
-    echo "errors found in logs"
+    echo "errors found in logs :("
+    kubectl -n "${NAMESPACE}" logs -l app.kubernetes.io/name=keycloak-controller
     exit 1
 else
-    echo "no errors found in logs"
+    echo "no errors found in logs :)"
+    kubectl -n "${NAMESPACE}" logs -l app.kubernetes.io/name=keycloak-controller
 fi
