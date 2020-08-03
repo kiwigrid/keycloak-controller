17a96268e735fe7e634e8a8a51484fdb2de0dcaf
André Bauer <monotek23@gmail.com>
2020-08-03 12:32:56 +0200

    removed travis
    Signed-off-by: André Bauer <monotek23@gmail.com>

--- .github/docker-build.sh
+++ .github/docker-build.sh
@@ -6,14 +6,6 @@ set -o errexit
 
 DOCKER_TAG="keycloak-controller:ci-snapshot"
 
-echo -e "\n##### install mvn & java #####\n"
-sudo apt-get update
-sudo apt-get install -y maven openjdk-11-jdk
-
-echo -e "\n##### build keycloak-controller #####\n"
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
-mvn package --quiet
-
 echo -e "\n##### create docker image with tag ${DOCKER_TAG} #####\n"
 docker build -t "${DOCKER_TAG}" ./target
 
